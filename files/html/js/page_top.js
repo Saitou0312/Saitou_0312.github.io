d6cd7ffad8670e685a95ac92996d8d7a6237df72
Saitou0312 <emma_1980_2010@yahoo.co.jp>
2021-10-27 07:34:53 +0900

    Create page_top.js

---
+++ files/html/js/page_top.js
@@ -0,0 +1,34 @@
+function PageTopCheck() {
+    var winScrollTop = $(this).scrollTop();
+    var secondTop = $('#area').offset().top - 150;
+    if (winScrollTop >= secondTop) {
+        $('.js-scroll').removeClass('scroll-view');
+        $('.js-pagetop').addClass('scroll-view');
+    } else {
+        $('.js-scroll').addClass('scroll-view');
+        $('.js-pagetop').removeClass('scroll-view');
+    }
+}
+
+$('.scroll-top a').click(function() {
+    var elmHash = $(this).attr('href');
+    if (elmHash == 'area') {
+        var pos = $(elmHash).offset().top;
+        $('body, html').animate({
+            scrollTop: pos
+        }, pos, 'easeInOutQuint');
+    } else {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutQuint');
+    }
+    return false;
+});
+
+$(window).scroll(function() {
+    PageTopCheck();
+});
+
+$(window).on('load', function() {
+    PageTopCheck();
+});
